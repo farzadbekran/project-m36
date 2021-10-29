94e2ae2684c23bdd8184b29679f7c11c501d8041
Farzad <farzadbekran@gmail.com>
2021-10-30 03:29:48 +0330

    added a message to main to ensure the right server is running

--- src/bin/ProjectM36/Server/project-m36-server.hs
+++ src/bin/ProjectM36/Server/project-m36-server.hs
@@ -1,9 +1,11 @@
 import ProjectM36.Server
 import ProjectM36.Server.ParseArgs
 import System.Exit (exitSuccess, exitFailure)
+import Text.Fuzzy
 
 main :: IO ()
 main = do
+  putStrLn "Fuzzy Included!"
   serverConfig <- parseConfig
   ret <- launchServer serverConfig Nothing
   if ret then exitSuccess else exitFailure
